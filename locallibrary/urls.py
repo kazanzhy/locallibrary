69bcd3c80f0c58d78dbf096ab10b8b45de0a6398
kazanzhy <dkazanzhy@gmail.com>
2018-06-30 01:09:35 +0300

    Changed urls

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    re_path(r'^catalog/', include('catalog.urls')),
+    re_path(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+

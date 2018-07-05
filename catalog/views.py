9fbfefacc5ec88c5d6f01869ba266f9c9909d138
kazanzhy <dkazanzhy@gmail.com>
2018-07-05 22:39:14 +0300

    Working with sessions

--- catalog/views.py
+++ catalog/views.py
@@ -11,6 +11,9 @@ def index(request):
     # Генерация "количеств" некоторых главных объектов
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
@@ -22,6 +25,7 @@ def index(request):
                 'num_instances_available':num_instances_available,
                 'num_authors':num_authors,
                 'num_genres': num_genres,
+                'num_visits':num_visits,
                 }
     return render(request, 'index.html', context= context)
 

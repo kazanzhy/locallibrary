78d5d0d1d6f0963ea4e877776a71244d0be2385f
kazanzhy <dkazanzhy@gmail.com>
2018-07-09 18:41:42 +0300

    changed urls

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^accounts/', include('django.contrib.auth.urls')),
     re_path(r'^catalog/', include('catalog.urls')),
     re_path(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

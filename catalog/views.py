ed7756d059d24d33e2bb808011017fbd171e5621
kazanzhy <dkazanzhy@gmail.com>
2018-07-10 00:35:09 +0300

    Authorization

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 from .models import Book, Author, BookInstance, Genre
 

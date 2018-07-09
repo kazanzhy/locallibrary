9d8af22145cea4a2afe1b4ada6748d989187ddc0
kazanzhy <dkazanzhy@gmail.com>
2018-07-10 01:11:29 +0300

    Added relation betwen User and BookInstance models

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,6 +11,8 @@ admin.site.register(Author, AuthorAdmin)
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
@@ -19,13 +21,13 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('display_book', 'status', 'due_back', 'id')
+    list_display = ('display_book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, 
             {'fields': ('book','imprint', 'id')}),
         ('Availability', 
-            {'fields': ('status', 'due_back')}),
+            {'fields': ('status', 'due_back', 'borrower')}),
     )
 
 
